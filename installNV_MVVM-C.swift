0bcde9861d460c2c23344e9281b6f346544fcd3a
Nikilicious <nikhilvinodmec@gmail.com>
2022-05-12 18:45:18 +0530

    ReOrganized the files for the installations

--- installNV_MVVM-C.swift
+++ installNV_MVVM-C.swift
@@ -10,77 +10,40 @@
 
 import Foundation
 
-struct Constants {
+let templateName = "NV_MVVM-C.xctemplate"
+let destinationRelativePath = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/iOS/Application"
 
-    struct CommandLineValues {
-        static let yes = "YES"
-        static let no = "NO"
-    }
-    struct File {
-        static let templateName = "NV_MVVM-C.xctemplate"
-        static let destinationRelativePath = "/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/iOS/Application"
-    }
-
-    struct Messages {
-        static let successMessage = "✅  Template was installed succesfully 🎉. Enjoy it 🙂"
-        static let successfullReplaceMessage = "✅  The Template has been replaced for you with the new version 🎉. Enjoy it 🙂"
-        static let errorMessage = "❌  Ooops! Something went wrong 😡"
-        static let exitMessage = "Bye Bye 👋"
-        static let promptReplace = "That Template already exists. Do you want to replace it? (YES or NO)"
-    }
-
-    struct Blocks {
-        static let printSeparator = { print("====================================") }
-    }
-}
-
-
-func printToConsole(_ message:Any){
-    Constants.Blocks.printSeparator()
+func printInConsole(_ message:Any){
+    print("====================================")
     print("\(message)")
-    Constants.Blocks.printSeparator()
+    print("====================================")
 }
 
 func moveTemplate(){
+
+    let fileManager = FileManager.default
+    let destinationPath = destinationRelativePath
     do {
-        let fileManager = FileManager.default
-        let destinationPath = bash(command: "xcode-select", arguments: ["--print-path"]).appending(Constants.File.destinationRelativePath)
-        printToConsole("Template will be copied to: \(destinationPath)")
-        if !fileManager.fileExists(atPath: "\(destinationPath)/\(Constants.File.templateName)"){
-            try fileManager.copyItem(atPath: Constants.File.templateName, toPath: "\(destinationPath)/\(Constants.File.templateName)")
-            printToConsole(Constants.Messages.successMessage)
+        if !fileManager.fileExists(atPath:"\(destinationPath)/\(templateName)"){
 
-        } else {
-            print(Constants.Messages.promptReplace)
-            var input = ""
-            repeat {
-                guard let textFormCommandLine = readLine(strippingNewline: true) else {
-                    continue
-                }
-                input = textFormCommandLine.uppercased()
-            } while(input != Constants.CommandLineValues.yes && input != Constants.CommandLineValues.no)
+            try fileManager.copyItem(atPath: templateName, toPath: "\(destinationPath)/\(templateName)")
 
-            if input == Constants.CommandLineValues.yes {
-                try replaceItemAt(URL(fileURLWithPath: "\(destinationPath)/\(Constants.File.templateName)"), withItemAt: URL(fileURLWithPath: Constants.File.templateName))
-                printToConsole(Constants.Messages.successfullReplaceMessage)
-            } else {
-                print(Constants.Messages.exitMessage)
-            }
+            printInConsole("✅  Template installed succesfully 🎉. Enjoy it 🙂")
+
+        }else{
+
+            try _ = fileManager.replaceItemAt(URL(fileURLWithPath:"\(destinationPath)/\(templateName)"), withItemAt: URL(fileURLWithPath:templateName))
+
+            printInConsole("✅  Template already exists. So has been replaced succesfully 🎉. Enjoy it 🙂")
         }
     }
-
     catch let error as NSError {
-        printToConsole("\(Constants.Messages.errorMessage) : \(error.localizedFailureReason!)")
+        printInConsole("❌  Ooops! Something went wrong 😡 : \(error.localizedFailureReason!)")
     }
 }
 
-func replaceItemAt(_ url: URL, withItemAt itemAtUrl: URL) throws {
-    let fileManager = FileManager.default
-    try fileManager.removeItem(at: url)
-    try fileManager.copyItem(atPath: itemAtUrl.path, toPath: url.path)
-}
-
-func shell(launchPath: String, arguments: [String]) -> String {
+func shell(launchPath: String, arguments: [String]) -> String
+{
     let task = Process()
     task.launchPath = launchPath
     task.arguments = arguments
